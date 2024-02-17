8bf29b424e11278350bb9bd4315ebf6f1e883a97
JeffreyDinackus <jeffreydinackus@proton.me>
2024-02-17 14:17:11 -0500

    adding comments

---
+++ backend/api.js
@@ -0,0 +1,16 @@
+
+
+
+// connect
+
+// build api for front end
+
+// put values in DB
+
+// query db for values
+
+// send values to llm
+
+// take in value from llm
+
+// send value back to front from llm 
