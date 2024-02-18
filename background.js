6bda8a0fbf3389a0926f9fd19ad78f5616670e69
JeffreyDinackus <jeffreydinackus@proton.me>
2024-02-17 19:50:17 -0500

    trying to build front end

---
+++ background.js
@@ -0,0 +1,15 @@
+// Listen for messages from the React frontend
+// Example of a simple user data object
+const user = {
+  username: 'demo-user'
+};
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  // 2. A page requested user data, respond with a copy of `user`
+  if (message === 'get-user-data') {
+    const txt = document.documentElement.innerHTML;
+    sendResponse(txt);
+
+  }
+
+});
--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,24 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import Content from './Content';
 import 'bulma/css/bulma.min.css';
+import { useState } from "react";
 
 function App() {
-  // const [count, setCount] = useState(0)
 
-  function run() {
-    console.log("xd")
+  // 1. Send a message to the service worker requesting the user's data
+  function x() {
+    chrome.runtime.sendMessage('get-user-data', (response) => {
+      // 3. Got an asynchronous response with the data from the service worker
+      console.log('received user data', response);
+    })
   }
-
 
   return (
     <>
       {/* <div style={{ backgroundColor: 'black' }}> */}
       <Navbar />
-      <button onClick={run} style={{ margin: "10px" }} className='button is-danger'>Run App</button>
+
+      <button onClick={x}>Get HTML from Tab</button>
 
       <Content />
       <Footer />
